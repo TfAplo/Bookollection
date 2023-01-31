dc2d8b56992845149e5a41d0c50eeb22dd6d012f
Evan <evanbeaufreton@gmail.com>
2023-01-31 11:21:13 +0100

    encore

--- home.php
+++ home.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_home.css">
     <title>Document</title>
 </head>
 <body>
-    <h1> deuxième page</h1>
+    <header>
+        <h1>Bookollection</h1>
+        <div id="bouton_header">
+            <button>Ma collection</button>
+            <button>Actualités</button>
+            <button>Recherche</button>
+        </div>
+    </header>
+
     <?php
         // recupere les informations entrées dans le formulaire
         $name = $_POST["username"];

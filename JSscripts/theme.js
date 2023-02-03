6c99c4c9f23f87b25d792084d494469318fdc553
Tom <tomfouquet1@gmail.com>
2023-02-03 21:20:21 +0100

    fin du changement de theme avec cookie

--- JSscripts/theme.js
+++ JSscripts/theme.js
@@ -1,16 +1,59 @@
+//recupere la collection de tous les a
 let a = document.querySelectorAll("a");
-let root = document.querySelector("root")
+
+//si l'utilisateur n'est jamais venu: cookie mis en sombre
+if (document.cookie == ""){
+    document.cookie = "themeCookie=Dark"
+}
+
+//raccourci pour ajouter des variables au style du document
+let des = document.documentElement.style;
+
+
+function setToDark() {
+    // change les variables de couleur pour le theme sombre
+    des.setProperty('--secondary', 'rgb(48, 52, 67)');
+    des.setProperty('--third', 'rgb(62, 67, 87)');
+    des.setProperty('--fontcolor', 'rgb(229, 226, 226)');
+}
+
+function setToBright() {
+    // change les variables de couleur pour le theme clair
+    des.setProperty('--secondary', 'rgb(229, 226, 226)');
+    des.setProperty('--third', 'rgb(177, 177, 177)');
+    des.setProperty('--fontcolor', 'rgb(0, 0, 0)');
+}
+
+//initialise les ouleurs en fonction du theme de l'utilisateur de sa derniere visite
+if (document.cookie.split(",")[0] == "themeCookie=Dark") {
+    setToDark();
+} else {
+    setToBright();
+}
+
+function themeCookieChange(element) {
+    //change le cookie de theme et lui ajoute une durée de 30 jours pour s'en souvenir
+    let expires = (new Date(Date.now()+ 86400*30)).toUTCString();
+    if (document.cookie == "themeCookie=Bright" && element.classList.contains("sombre")) {
+        document.cookie = "themeCookie=Dark; expires=" + expires + ";path=/;"
+    } else {
+        document.cookie = "themeCookie=Bright; expires=" + expires + ";path=/;"
+    }
+}
+
+function colorChange(element) {
+    //change le theme du document
+    if (document.cookie == "themeCookie=Bright" && element.classList.contains("clair") ) {
+        setToBright();
+    } else if (document.cookie == "themeCookie=Dark" && element.classList.contains("sombre")) {
+        setToDark();
+    }
+}
 
 for (let element of a) {
     element.addEventListener("click", () => {
-        if (element.classList.contains("clair")) {
-            document.documentElement.style.setProperty('--secondary', 'rgb(229, 226, 226)');
-            document.documentElement.style.setProperty('--third', 'rgb(200, 200, 226)');
-            document.documentElement.style.setProperty('--fontcolor', 'rgb(0, 0, 0)');
-        } else {
-            document.documentElement.style.setProperty('--secondary', 'rgb(48, 52, 67)');
-            document.documentElement.style.setProperty('--third', 'rgb(62, 67, 87)');
-            document.documentElement.style.setProperty('--fontcolor', 'rgb(229, 226, 226)');
-        }
+        //evnenListeners ajoutés pour changer le theme si les a sont cliqués
+        themeCookieChange(element);
+        colorChange(element);
     })
 }

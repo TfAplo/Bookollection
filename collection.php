b94ae6607b2404dd8a139e07f6f25e87f2770e99
flo <florianbiardeau@gmail.com>
2023-02-13 15:42:10 +0100

    test_largeur_vh_%

--- collection.php
+++ collection.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style_collection.css">
-    <link rel="icon" href="images\ico.png" type="image/png">
+    <link rel="icon" href="images\ico-removebg-preview.png" type="image/png">
     <title>MaCollection - Bookollection</title>
 </head>
 <body>
     <header>
         <nav>
-            <img alt="Logo de MyBookollection" src="images/ico.png"> 
+            <img alt="Logo de MyBookollection" src="images/ico-removebg-preview.png"> 
             <input class="nav_button" type="button" value="Ma collection">
             <input class="nav_button" type="button" value="Actualités">
             <input class="nav_button" type="button" value="Recherche">
@@ -26,28 +26,22 @@
             <form name="form" id="filtre">
                 <h2>Filtres</h2>
                 <label for="" id="genre">
-                    Genre :
-                    <select id="select_genre">
-                        <option value="roman">Roman</option>
-                        <option value="bd">Bande Dessinée</option>
-                        <option value="poésie">Poésie</option>
-                        <option value="autobiographie">Autobiographie</option>
-                    </select>
+                    Genre :<br>
+                    <label class="label_genre"><input type="checkbox" class="input_genre" name="genre[]" value="roman"> Roman</label><br>
+                    <label class="label_genre"><input type="checkbox" class="input_genre" name="genre[]" value="bd"> Bande Dessinée</label><br>
+                    <label class="label_genre"><input type="checkbox" class="input_genre" name="genre[]" value="poésie"> Poésie</label><br>
+                    <label class="label_genre"><input type="checkbox" class="input_genre" name="genre[]"  value="autobiographie"> Autobiographie</label>
                 </label>
                 <br>
                 <br>
                 <label for="" id="registre">
-                    Registre :
-                    <select id="select_registre">
-                        <option value="fantastique">Fantastique</option>
-                        <option value="policier">Policier</option>
-                        <option value="comique">Comique</option>
-                        <option value="pique">Epique</option>
-                        <option value="tragique">Tragique</option>
-                    </select>
+                    Registre :<br>
+                    <label class="label_registre"><input type="checkbox" class="input_registre" name="registre[]" value="fantastique"> Fantastique</label><br>
+                    <label class="label_registre"><input type="checkbox" class="input_registre" name="registre[]" value="policier"> Policier</label><br>
+                    <label class="label_registre"><input type="checkbox" class="input_registre" name="registre[]" value="comique"> Comique</label><br>
+                    <label class="label_registre"><input type="checkbox" class="input_registre" name="registre[]"  value="epique"> Epique</label><br>
+                    <label class="label_registre"><input type="checkbox" class="input_registre" name="registre[]"  value="tragique"> Tragique</label><br>
                 </label>
-                <br>
-                <br>
                 <label for="" id="lu">
                     <input type="checkbox" name="lu" id="checkbox_lu">
                     Lu
@@ -63,6 +57,16 @@
         
         <div id="droite">
             <input type="text" id="input_recherche" placeholder="Rechercher un livre, un auteur, ...">
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
+            <span class="livre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis aperiam tempore id suscipit illo voluptatibus inventore officiis quisquam laudantium officia distinctio quibusdam rerum, obcaecati necessitatibus quia accusantium. Exercitationem, aut incidunt</span>
         </div>
 
     </div>

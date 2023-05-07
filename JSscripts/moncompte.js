a5cc7d1a4c9c8783add4116c9c623fffa2aad27e
Evan <evanbeaufreton@gmail.com>
2023-05-07 19:43:40 +0200

    olalal cocola

---
+++ JSscripts/moncompte.js
@@ -0,0 +1,23 @@
+const categories = document.querySelectorAll('.categorie');
+
+categories.forEach(categorie => {
+  const toggle = categorie.querySelector('.toggle');
+  const contenu = categorie.querySelector('.contenu');
+
+  toggle.addEventListener('click', () => {
+    categories.forEach(c => {
+      if (c !== categorie) {
+        c.classList.remove('actif');
+        c.querySelector('.contenu').style.maxHeight = null;
+      }
+    }); // sheeeeesh
+
+    categorie.classList.toggle('actif');
+
+    if (categorie.classList.contains('actif')) {
+      contenu.style.maxHeight = contenu.scrollHeight + 'px';
+    } else {
+      contenu.style.maxHeight = null;
+    }
+  });
+});
---
+++ moncompte.php
@@ -0,0 +1,63 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" contenu="IE=edge">
+    <meta name="viewport" contenu="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images\ico-removebg-preview.png" type="image/png">
+    <link rel="stylesheet" href="styles/moncompte.css">
+    <title>Mon Compte - Bookollection</title>
+    </head>
+<body>
+    <div class="menu">
+        <div class="categorie">
+            <h2 class="toggle">Profil</h2>
+            <div class="contenu">
+            <p> Evan Beaufreton, ta race flo</p>
+            </div>
+        </div>
+
+        <div class="categorie">
+            <h2 class="toggle">Sécurité</h2>
+            
+            <div class="contenu">
+            <p>securité é é securité é é</p>
+            
+            </div>
+        </div>
+
+        <div class="categorie">
+            <h2 class="toggle">Soutenir</h2>
+
+            <div class="contenu">
+            <p>na wash miné na wash minéné okay na wash mi né né wash mi né né </p>
+
+            </div>
+        </div>
+
+        <div class="categorie">
+            <h2 class="toggle">Assistance</h2>
+
+            <div class="contenu">
+            <p>Contenu de la catégorie Assistance</p>
+            </div>
+        </div>
+
+        <div class="categorie">
+            <h2 class="toggle">A Propos</h2>
+            
+            <div class="contenu">
+            <a href="www.google.com">Google</a>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="JSscripts/moncompte.js"></script>
+    <script src="JSscripts/theme.js"></script>
+</body>
+</html>
